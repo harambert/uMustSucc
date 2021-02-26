27ef4c9d5099720141a1c6a7b77a33a797f0e85c
ikcikoR <48479629+ikcikoR@users.noreply.github.com>
2021-02-26 17:46:04 +0100

    Player controls on WSAD

--- Player.gd
+++ Player.gd
@@ -10,10 +10,10 @@ func _ready():
 	pass
 	
 func _physics_process(delta):
-	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	inputVector.x = Input.get_action_strength("rightwards") - Input.get_action_strength("leftwards")
 	velo.x = inputVector.x * MAXSPD
 	move_and_slide(velo, Vector2.UP)
 	if !is_on_floor():
 		velo.y += GRAV
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_just_pressed("upwards"):
 		velo.y = - 100
