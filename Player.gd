69f60e8a1de7b0d24d851b8e377a76afdb067cf0
harambert <dionisioalbertjohn@gmail.com>
2021-02-27 01:20:36 +0800

    hello

--- Player.gd
+++ Player.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
 const GRAV = 5
-const MAXSPD = 50
+const MAXSPD = 60
 
 var velo = Vector2.ZERO
 var inputVector = Vector2.ZERO
+
+onready var sprite = $Sprite
+onready var animation = $AnimationPlayer
 
 func _ready():
 	pass
@@ -15,5 +18,19 @@ func _physics_process(delta):
 	move_and_slide(velo, Vector2.UP)
 	if !is_on_floor():
 		velo.y += GRAV
-	if Input.is_action_just_pressed("ui_up"):
-		velo.y = - 100
+		animation.play("Jump")
+	elif inputVector.x == 0:
+		animation.play("Idle")
+	else:
+		animation.play("Run")
+	
+	if is_on_floor():
+		velo.y = 10
+		if Input.is_action_just_pressed("ui_up"):
+			velo.y = -125
+	
+	if inputVector.x > 0:
+		sprite.flip_h = false
+	if inputVector.x < 0:
+		sprite.flip_h = true
+	
